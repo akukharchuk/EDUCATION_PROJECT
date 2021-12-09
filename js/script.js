c838b1c2ee3871ccf88a50f81a583844ed4ffd9b
Artem <example@gmail.com>
2021-12-09 16:32:14 +0200

    my first commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Здоров, ${user}`);
+
+/* alert('Hello'); */
+
+/* const result = confirm("Are you here?");
+console.log(result); */
+
+/* const answer = +prompt ('Скільки грошей тобі дати?', '1000');
+console.log(answer + 20); */
+
+/* const answers = [];
+answers [0] = prompt("Яке ваше ім'я?", "");
+answers [1] = prompt("Яке ваше прізвище?", "");
+answers [2] = prompt("Скільки вам років?", "");
+
+document.write(answers); */
+
+
+
+
+
+
+/* let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = 'Alex'; */
+
+/* const bool = true;
+const bool = false; */
+
+/* console.log(something); */
+
+/* let und;
+console.log(und);
+
+const obj = {
+    name: 'Jonh',
+    age: 25,
+    isMarried: false
+};
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]); */
+
+/* console.log(obj.name); */
+/* console.log(obj['name']); */
+
+
+
+/* a=15;
+console.log(a);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+obj.a = 10;
+
+console.log(obj);
+
+console.log(name);
+var name = 'Ivan';
+
+{
+    let result = 50;
+}
+
+console.log(result); */
